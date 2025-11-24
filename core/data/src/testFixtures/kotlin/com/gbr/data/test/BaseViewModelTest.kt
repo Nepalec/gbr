5affb8edb35e384d8aa44ed861f404e64162a0d9
Ivan <i1sharov@gmail.com>
2025-11-24 12:25:46 +1100

    get rid of java folders

--- core/data/src/testFixtures/kotlin/com/gbr/data/test/BaseViewModelTest.kt
+++ core/data/src/testFixtures/kotlin/com/gbr/data/test/BaseViewModelTest.kt
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith
 /**
  * Base class for ViewModel integration tests.
  * Provides common setup and teardown for all ViewModel tests.
- * 
+ *
  * Usage:
  * ```kotlin
  * class MyViewModelTest : BaseViewModelTest() {
@@ -26,38 +26,38 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 abstract class BaseViewModelTest {
-    
+
     companion object {
         @JvmStatic
         lateinit var fixture: TestDatabaseFixture
-        
+
         @BeforeClass
         @JvmStatic
         fun setupClass() {
             fixture = TestDatabaseFixture.getInstance()
         }
-        
+
         @AfterClass
         @JvmStatic
         fun tearDownClass() {
             TestDatabaseFixture.cleanup()
         }
     }
-    
+
     // Convenience properties for subclasses
     protected val textsRepository: TextsRepository
         get() = fixture.textsRepository
-    
+
     protected val loadBookDetailUseCase: LoadBookDetailUseCase
         get() = fixture.loadBookDetailUseCase
-    
+
     protected val testGitabaseId: GitabaseID
         get() = fixture.getTestGitabaseId()
 
     /**
      * Initializes gitabases by scanning the test folder.
      * Call this in your test's runTest block before accessing gitabases.
-     * 
+     *
      * Example:
      * ```kotlin
      * @Test
